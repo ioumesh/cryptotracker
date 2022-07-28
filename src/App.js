3a25d4381203fd5fe8c3e6ff6c87993f05c14cd3
ioumesh <umeshkumar@zingbus.com>
2022-07-28 23:48:29 +0530

    feat:dark theme added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Coin from "./pages/Coin/Coin";
+import Header from "./components/Header/Header";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  App: {
+    backgroundColor: "#14161a",
+    color: "#ffffff",
+    minHeight: "100vh",
+  },
+});
 
 function App() {
-  return <div className="App">Hello react</div>;
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="coins">
+              <Route path=":id" element={<Coin />} />
+            </Route>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
